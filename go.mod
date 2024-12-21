7a0fbdadd698cb46ea640644003642ff52815593
Jack112-Create <jackconroy777@gmail.com>
2024-12-21 15:18:29 +0000

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jack112-create/tidy-go-mod
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/routes", GetRoutes)
+
+	fmt.Println("Server listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func GetRoutes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	resp, err := http.Get("https://galway-bus.apis.ie/api")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	sb := string(body)
+	err = json.NewEncoder(w).Encode(sb)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	log.Printf(sb)
+}
